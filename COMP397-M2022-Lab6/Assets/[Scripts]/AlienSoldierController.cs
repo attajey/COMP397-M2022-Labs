573b337714342a131b6c1d1ffd6731262c88dde1
Atta Jirofty <jirofty76@gmail.com>
2022-06-17 15:55:04 -0400

    Animation Transition Added

---
+++ COMP397-M2022-Lab6/Assets/[Scripts]/AlienSoldierController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlienSoldierController : MonoBehaviour
+{
+    public Animator animatorController;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // animatorController.GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            animatorController.SetInteger("AnimationState", 0);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            animatorController.SetInteger("AnimationState", 1);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            animatorController.SetInteger("AnimationState", 2);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            animatorController.SetInteger("AnimationState", 2);
+        }
+    }
+}
