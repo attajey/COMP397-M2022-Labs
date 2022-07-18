49658f8526a417203ba4a2eedac53aacc33cc9eb
Atta Jirofty <jirofty76@gmail.com>
2022-07-18 14:59:48 -0400

    Added Project Files for Lab8
    Start Menu Fixed for Galaxy S10+

---
+++ COMP397-M2022-Lab8/Assets/[Scripts]/HealthBarController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class HealthBarController : MonoBehaviour
+{
+    public Slider bar;
+    public TMP_Text healthLabel;
+    [Range(0, 100)]
+    public int healthValue = 100;
+
+    private int startingHealthValue;
+    void Start()
+    {
+        startingHealthValue = healthValue;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthValue != startingHealthValue)
+        {
+            startingHealthValue = healthValue;
+            bar.value = healthValue;
+            OnValueChanged();
+        }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        //_ = healthValue < 0 ? healthValue = 0 : healthValue -= damage;
+        healthValue -= damage;
+        if (healthValue < 0)
+        {
+            healthValue = 0;
+        }
+    }
+
+
+    public void OnValueChanged()
+    {
+        healthLabel.text = bar.value.ToString();
+
+    }
+}
