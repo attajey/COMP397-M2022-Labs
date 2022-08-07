6b9c6c47d779f727cf99aa16e46a08fb34163efa
Atta Jirofty <jirofty76@gmail.com>
2022-08-07 14:11:50 -0400

    Server & Client Update Added

---
+++ COMP397-M2022-Lab10/Assets/[Scripts]/NetworkPlayerBehaviour.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class NetworkPlayerBehaviour : NetworkBehaviour
+{
+    private NetworkVariable<float> verticalPosition = new NetworkVariable<float>();
+    private NetworkVariable<float> horizontalPosition = new NetworkVariable<float>();
+
+    // Keeps track of local positions
+    private float localHorizontal;
+    private float localVertical;
+
+
+    void Start()
+    {
+        RandomSpawnPosition();
+    }
+
+    void Update()
+    {
+        if (IsServer)
+        {
+            // Server Update
+            ServerUpdate();
+        }
+        if (IsClient && IsOwner)
+        {
+            // Client Update
+            ClientUpdate();
+        }
+    }
+
+    void ServerUpdate()
+    {
+        transform.position = new Vector3(transform.position.x + horizontalPosition.Value,
+            transform.position.y, transform.position.z + verticalPosition.Value);
+    }
+
+    public void RandomSpawnPosition()
+    {
+        var x = Random.Range(-10.0f, 10.0f);
+        var z = Random.Range(-10.0f, 10.0f);
+        transform.position = new Vector3(x, 1.0f, z);
+    }
+
+    public void ClientUpdate()
+    {
+        var horizontal = Input.GetAxis("Horizontal") * Time.deltaTime;
+        var vertical = Input.GetAxis("Vetical") * Time.deltaTime;
+
+        // Network Update - I need a network var for this 
+        if (localHorizontal != horizontal || localVertical != vertical)
+        {
+            localHorizontal = horizontal;
+            localVertical = vertical;
+
+            // Update the client position on the network
+            UpdateClientPositionServerRpc(horizontal, vertical);
+        }
+
+    }
+
+    [ServerRpc]
+    public void UpdateClientPositionServerRpc(float horizontal, float vertical)
+    {
+        horizontalPosition.Value = horizontal;
+        verticalPosition.Value = vertical;
+    }
+}
